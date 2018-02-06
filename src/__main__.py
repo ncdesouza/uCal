4bc9babc3556a72392f7b8983a822eb61fafb8c2
Nicholas De Souza <nicholas.desouza@uoit.net>
2018-02-06 18:46:40 -0500

    prepare for submission as Machine Learning Assignment #1

---
+++ src/__main__.py
@@ -0,0 +1,35 @@
+import json
+import sys
+
+import os
+
+from ucal import UCal
+from gui import GUI
+
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath("..")
+
+    return os.path.join(base_path, relative_path)
+
+def main():
+    ucal = UCal()
+
+    try:
+        with open(resource_path('save.json'), 'r') as f:
+            credentials = json.load(f)
+    except IOError:
+        credentials = {'username': None, 'password': None}
+
+    gui = GUI(ucal.start, ucal.clear_all, credentials['username'], credentials['password'])
+    gui.master.title('uCal - Import your UOIT schedule to google calendar')
+    gui.mainloop()
+
+
+if __name__ == '__main__':
+    main()
